30a78fe0d2ef245c98ac87d2faacb1ffd7ab8dc6
pl19970 <awesomedraxta@gmail.com>
2023-07-17 20:17:13 +0200

    inital commit for this project

--- src/test/java/service/ShapeServiceTest.java
+++ src/test/java/service/ShapeServiceTest.java
@@ -24,6 +24,7 @@ import java.util.*;
 
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class ShapeServiceTest {
 
@@ -44,9 +45,8 @@ public class ShapeServiceTest {
 
     private Square square;
 
-    private String PATH = "src/main/resources/obj.json";
-    FileSystem fileSystem;
-    String fileName;
+
+
 
     @Before
     public void init() {
@@ -58,35 +58,17 @@ public class ShapeServiceTest {
         rectangle = shapeFactory.createRectangle(4, 5);
         square = shapeFactory.createSquare(4);
         shapeList = new ArrayList<>(List.of(circle, rectangle, square));
-        fileSystem = Jimfs.newFileSystem(Configuration.windows());
-        fileName = "test.json";
     }
 
 
     @Test
-    public void test() throws URISyntaxException, IOException {
-        Path resourceFilePath = fileSystem.getPath("test.json");
-        Files.copy(getResourceFilePath(), resourceFilePath);
+    public void test() throws IOException {
+        FileSystem fileSystem = Jimfs.newFileSystem(Configuration.windows());
+        String fileName = "test.json";
+        Path pathToStore = fileSystem.getPath("");
+        fileRepository.create(pathToStore, fileName);
 
-        shapeService.exportShapes(shapeList, getResourceFilePath().toString());
-        String content = fileRepository.read(resourceFilePath);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonString = objectMapper.writerFor(new TypeReference<List<Shape>>() {
-        }).writeValueAsString(shapeList);
-
-
-        assertEquals(jsonString, content);
+        assertTrue(Files.exists(pathToStore));
     }
 
-    @Test
-    public void test1() throws URISyntaxException {
-        long result = shapeService.importShapes(getResourceFilePath().toString()).size();
-
-        assertEquals(3, result);
-    }
-
-
-    private Path getResourceFilePath() throws URISyntaxException {
-        return Paths.get(getClass().getClassLoader().getResource("test.json").toURI());
-    }
 }
